758864dc4c1ad1e37bd2734cf2a84ad3d64b32ed
A Pottinger <samp@gleap.org>
2019-03-25 11:47:16 -0700

    Created code-signing logic.

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,9 +1,11 @@
 mkdir all_os_release
 
+
 echo "=============================="
 echo "====   Getting CLI Setup  ===="
 echo "=============================="
 pip install awscli --upgrade --user
+
 
 echo "=============================="
 echo "====   Preparing Windows  ===="
@@ -11,11 +13,34 @@ echo "=============================="
 ant cross-build-windows
 zip -r all_os_release/windows.zip windows/work
 
+
 echo "=============================="
 echo "====  Preparing MacOSX    ===="
 echo "=============================="
+
+echo ">>> Building for Mac"
 ant cross-build-macosx
+
+echo ">>> Creating keychain for signing"
+security create-keychain -p $KEYCHAIN_PASS build.keychain
+security default-keychain -s build.keychain
+
+echo ">>> Unlocking keychain"
+security unlock-keychain -p $KEYCHAIN_PASS build.keychain
+
+echo ">>> Decrypting and registering certificate"
+openssl aes-256-cbc -K $encrypted_0d213ffbe1d5_key -iv $encrypted_0d213ffbe1d5_iv -in mac_developerID.cer.enc -out mac_developerID.cer -d
+security import mac_developerID.cer -k build.keychain -T /usr/bin/codesign
+
+echo ">>> Signing application"
+ant macosx-dist-sign
+
+echo ">>> Locking keychain"
+security lock-keychain build.keychain
+
+echo ">>> Creating zip"
 zip -r all_os_release/macosx.zip macosx/work
+
 
 echo "=============================="
 echo "==== Preparing Linux x86  ===="
@@ -23,17 +48,20 @@ echo "=============================="
 ant cross-build-linux-x64
 zip -r all_os_release/linux_x64.zip linux/work
 
+
 echo "=============================="
 echo "==== Preparing Linux ARM  ===="
 echo "=============================="
 ant cross-build-linux-aarch64
 zip -r all_os_release/linux_aarch64.zip linux/work
 
+
 echo "=============================="
 echo "====       Packing        ===="
 echo "=============================="
 TIMESTAMP=$(date -u "+%Y-%m-%d_%H-%M-%S")
 zip -r all_os_release_$TIMESTAMP.zip all_os_release
+
 
 echo "=============================="
 echo "====       Deploying      ===="
